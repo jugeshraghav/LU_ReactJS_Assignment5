865428c82d51bc421fde295f912306e27e15d6a8
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-07-18 14:27:45 +0530

    LU_ReactJS Assignment5 16/07/21

---
+++ src/Components/CardSection.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Card from './Card'
+import img1 from '../img1.jpg'
+import img2 from '../img2.png'
+import img3 from '../img3.jpg'
+
+
+const CardSection = () => {
+  return (
+    <section className="contact bg-success ">
+      <div className="container ">
+        <h2 className="text-white">
+          We love new friends!
+        </h2>
+        <div className="row">
+           <Card cardTitle="Orange" cardDescription="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+           buttonText="Pear" img={img1}/>
+           <Card cardTitle="Guava" cardDescription="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+           buttonText="Apple" img={img2}/>
+           <Card cardTitle="Mango" cardDescription="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+           buttonText="Pineapple" img={img3}/>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default CardSection
